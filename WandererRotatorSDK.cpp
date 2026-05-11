c50eb719e60ba9c4051f1a5d76b158d7044ba063
Nico <nitro.mail@web.de>
2026-05-11 11:32:34 +0200

    bump version

--- WandererRotatorSDK.cpp
+++ WandererRotatorSDK.cpp
@@ -47,7 +47,7 @@
 #include <dirent.h>
 #include <libudev.h>
 
-#define SDK_VERSION "1.3.3"
+#define SDK_VERSION "1.3.4"
 
 /* Import internal implementation for use in public C API */
 using namespace WandererRotator;
